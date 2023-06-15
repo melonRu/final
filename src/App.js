9a86cb8f1b5a3f7a0c630bd50e4b9c2806e0b420
melonRu <subin6605@naver.com>
2023-06-15 13:31:15 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState('Purple');
+
+  const handleInputChange = (e) => {
+    setBackgroundColor(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor,
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h1>Background Color: {backgroundColor}</h1>
+      <input type="text" onChange={handleInputChange} />
     </div>
   );
 }
